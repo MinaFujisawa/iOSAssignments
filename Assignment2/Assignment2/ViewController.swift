c2d82e73c714fc87696021cc2a7b2d98c5afc1ba
MinaFujisawa <fujimina37@gmail.com>
2017-09-20 10:49:37 -0700

    add asn2

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by MINA FUJISAWA on 2017/09/20.
+//  Copyright © 2017 MINA FUJISAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
