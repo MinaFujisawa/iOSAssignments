51aa7adbf9448c0401c98f2b50a602a6dc4e3cdb
MinaFujisawa <fujimina37@gmail.com>
2017-10-18 12:32:52 -0700

    add NetWorkingBasicJSON

---
+++ NetWorkingBasicJSON/NetWorkingBasicJSON/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NetWorkingBasicJSON
+//
+//  Created by MINA FUJISAWA on 2017/10/18.
+//  Copyright © 2017 MINA FUJISAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
